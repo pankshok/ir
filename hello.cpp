cbea6a74eb8acaca12e6091073b84f1b5861e9d5
pankshok <kulyov.pavel@gmail.com>
2015-07-25 01:58:30 +0300

    Add rotated and textured cube with funny background.

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,8 @@ using namespace video;
 using namespace gui;
 using namespace io;
 
+enum class Order {ASCENDING, DESCENDING};
+
 int main(int argc, char **argv) {
     IrrlichtDevice *device = createDevice(video::EDT_OPENGL,
                                           dimension2d<u32>(640, 480), 16,
@@ -21,12 +23,37 @@ int main(int argc, char **argv) {
     IGUIEnvironment *guienv = device->getGUIEnvironment();
 
     guienv->addStaticText(L"Hello world! This is the Irrlicht OpenGL renderer!",
-                          rect<s32>(10, 10, 260, 22), true);
+                          rect<s32>(10, 10, 260, 30), true);
+
+    IMeshSceneNode *wall = smgr->addCubeSceneNode();
+    wall->setMaterialFlag(EMF_LIGHTING, false);
+    wall->setMaterialTexture(0, driver->getTexture("./wall.jpg"));
 
     smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
 
+    int i = 0;
+    Order mode = Order::ASCENDING;
     while(device->run()) {
-        driver->beginScene(true, true, SColor(255, 100, 101, 140));
+        driver->beginScene(true, true, SColor(0, i, i, i));
+        if (mode == Order::ASCENDING)
+            if (i >= 254) {
+                mode = Order::DESCENDING;
+                i--;
+            } else {
+                i++;
+        } else {
+            if (mode == Order::DESCENDING) {
+                if (i <= 0) {
+                    mode = Order::ASCENDING;
+                    i++;
+                } else {
+                    i--;
+                }
+            }
+        }
+
+
+        wall->setRotation(wall->getRotation() + vector3df(0, 1, 0));
 
         smgr->drawAll();
         guienv->drawAll();
